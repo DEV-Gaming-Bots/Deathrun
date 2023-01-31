8bee9b7af7f17b7edbe1226d789a6c10493f3bdb
ItsRifter <d_thomas_smith30@hotmail.com>
2023-01-31 18:23:40 +0000

    Pawn + Controller

---
+++ code/Gameplay/Commands.cs
@@ -0,0 +1,14 @@
+﻿namespace Deathrun;
+
+
+public partial class DRGame
+{
+	public static bool Debugging { get; protected set; }
+
+	[ConCmd.Admin("dr.debug")]
+	public static void ToggleDebug(bool toggle)
+	{
+		Debugging = toggle;
+		Log.Info( $"Debug mode is {Debugging}" );
+	}
+}
---
+++ code/Gameplay/Game.cs
@@ -0,0 +1,28 @@
+﻿
+global using System;
+global using System.IO;
+global using System.Linq;
+global using System.Threading.Tasks;
+global using Sandbox;
+global using Sandbox.UI;
+global using Sandbox.UI.Construct;
+global using Deathrun.Player;
+
+
+namespace Deathrun;
+
+
+public partial class DRGame : GameManager
+{
+	public DRGame()
+	{
+	}
+
+	public override void ClientJoined( IClient client )
+	{
+		base.ClientJoined( client );
+
+		var pawn = new PlayerPawn(client);
+		client.Pawn = pawn;
+	}
+}
